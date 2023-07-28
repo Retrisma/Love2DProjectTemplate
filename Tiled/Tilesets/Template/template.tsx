1e5ebe5609ce9ad8b295bd2cba0a5435580bbed5
Retrisma <64330958+Retrisma@users.noreply.github.com>
2023-07-28 10:20:38 -0400

    set up for a script to automatically add tilesets

--- Tiled/Tilesets/Template/template.tsx
+++ Tiled/Tilesets/Template/template.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Template" tilewidth="16" tileheight="16" tilecount="121" columns="11">
- <image source="template.png" width="176" height="176"/>
+<tileset version="1.10" tiledversion="1.10.1" name="Template" tilewidth="16" tileheight="16" tilecount="121" columns="11">
+ <image source="Template.png" width="176" height="176"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
