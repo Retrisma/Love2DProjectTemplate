97eca7d964f0db5437ada1b3948245aaed64f208
Retrisma <64330958+Retrisma@users.noreply.github.com>
2024-06-04 10:50:18 -0400

    clamp weirdness

--- shaders.lua
+++ shaders.lua
@@ -15,13 +15,13 @@ basic.shader = love.graphics.newShader[[
     float light_source(vec2 screen_coords, vec2 light, float falloff) {
         float d = distance(screen_coords, world_transform(light));
         float intensity = 1.5 - (d / falloff);
-        return clamp(intensity, 1.0, 0.0);
+        return clamp(intensity, 0.0, 1.0);
     }
 
     vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
         vec4 pixel = Texel(texture, texture_coords);
         
-        float intensity = clamp(light_source(screen_coords, light_pos[0], 1000) + light_source(screen_coords, player_pos, 150), 0.4, 1.0);
+        float intensity = clamp(light_source(screen_coords, light_pos[0], 1000) + light_source(screen_coords, player_pos, 150), 0.0, 1.0);
 
         return pixel * intensity;
     }
